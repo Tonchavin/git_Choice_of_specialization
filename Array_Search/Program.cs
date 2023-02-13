e74abfe8202191b906d0840387373552ac1a4392
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-02-13 15:06:41 +1000

    add method Array_Search

--- Array_Search/Program.cs
+++ Array_Search/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string[] ArrayA = { "hi", "hello", "222", "world", ":)", "aaa" };
+MinArraySearch(ArrayA);
+
+string MinArraySearch(string[] a)
+{
+    string min = string.Empty;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i].Length <= 3)
+            min += "\"" + a[i] + "\"" + ",";
+    }
+    Write($"[{min}]");
+    return min;
+}
